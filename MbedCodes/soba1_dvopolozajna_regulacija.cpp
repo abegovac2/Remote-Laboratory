5c2d79341a73623505ebcf3200a6a7867ecf9d30
dzegz <85890327+dzegz@users.noreply.github.com>
2021-06-18 23:45:12 +0200

    Add files via upload
    Added beta version of first room example

---
+++ MbedCodes/soba1_dvopolozajna_regulacija.cpp
@@ -0,0 +1,151 @@
+//
+// Ugradbeni sistemi
+// SOBA1: Dvopoložajna regulacija temperature
+//
+
+#define SUBNTCVOLTAGE "theme/ntcvoltage"
+#define SUBRUNNINGTIME "theme/timewant"
+
+#define PUBHEATERSTATE "theme/heater"
+#define PUBRUNNINGTIME "theme/time"
+
+#include "mbed.h"
+
+#define MQTTCLIENT_QOS2 0
+
+#include "easy-connect.h"
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
+#include <string.h>
+
+Ticker check30min;
+Timer time_running;
+DigitalOut led1(p7);
+AnalogOut dc_dimmer(p18);
+float v_ntc;
+bool heater=false;
+int block_of_30min=0;
+bool mqqt_wants_time=false;
+
+
+void heater_regulation(MQTT::MessageData& md)
+{
+    /*
+    temperatura=1/0.04799 * ln(2.131*10^4/((v_ntc*673)/(3.3-v_ntc)))
+    look-up:
+    23C -> Vntc = 3.01305 V
+    25C -> Vntc = 2.98689 V
+    27C -> Vntc = 2.95861 V
+    */
+    MQTT::Message &message = md.message;
+    printf("Napon na NTC %.*s\r\n", message.payloadlen, (char*)message.payload);
+    v_ntc=atof((char*)message.payload);
+    printf("Stanje: ");
+
+    if(v_ntc < 2.98689){ // > 25 C
+        heater=false;
+        printf("25 C+ | Grijac: OFF\n");
+    }
+    else if(v_ntc < 3.03722){ // [21 C, 25 C]
+        heater=false;
+        printf("23+-2 C | Grijac: OFF\n");
+    }
+    else{ // < 21 C
+        heater=true;
+        printf("21 C- | Grijac: ON\n");
+    }
+}
+
+void wantTime(MQTT::MessageData& md){
+    // kod
+    mqqt_wants_time=true;
+}
+
+int running_time_in_minutes(){
+    return block_of_30min*30+time_running.read()/60;
+}
+
+void check30min_fun(){
+    time_running.stop();
+    block_of_30min++;
+    time_running.start();
+}
+
+int main(int argc, char* argv[])
+{
+    time_running.start();
+    check30min.attach(&check30min_fun, 30*60);
+    printf("Ugradbeni sistemi\r\n");
+    printf("SOBA 1: Dvopoložajna regulacija\r\n\r\n");
+
+    NetworkInterface *network;
+    network = NetworkInterface::get_default_instance();
+    
+    if (!network) {
+        return -1;
+    }
+
+    MQTTNetwork mqttNetwork(network);
+
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+
+    const char* hostname = "broker.hivemq.com";
+    int port = 1883;
+    printf("Connecting to %s:%d\r\n", hostname, port);
+    int rc = mqttNetwork.connect(hostname, port);
+    if (rc != 0)
+        printf("rc from TCP connect is %d\r\n", rc);
+
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = "proizvoljno";
+    data.username.cstring = "";
+    data.password.cstring = "";
+    if ((rc = client.connect(data)) != 0)
+        printf("rc from MQTT connect is %d\r\n", rc);
+
+    if ((rc = client.subscribe(SUBNTCVOLTAGE, MQTT::QOS2, heater_regulation)) != 0)//
+        printf("rc from MQTT subscribe is %d\r\n", rc);
+        
+    if ((rc = client.subscribe(SUBRUNNINGTIME, MQTT::QOS2, wantTime)) != 0)//
+        printf("rc from MQTT subscribe is %d\r\n", rc);
+        
+    MQTT::Message message;
+
+    // QoS 0
+    char buf[100];
+    bool old_heater=false;
+    while(1) {
+        led1 = heater; 
+        if(heater) dc_dimmer=1; //3.3 na ulaz dimmera
+        else dc_dimmer=0;
+        
+        if (old_heater!=heater) {
+            old_heater=heater;
+            sprintf(buf, "{\"Stanje grijaca\": %d}", heater);
+            message.qos = MQTT::QOS0;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void*)buf;
+            message.payloadlen = strlen(buf);
+            rc = client.publish(PUBHEATERSTATE, message);
+        }
+        if(mqqt_wants_time){
+            mqqt_wants_time=false;
+            sprintf(buf, "{\"Sistem u pripravnosti\": %d}", running_time_in_minutes);
+            message.qos = MQTT::QOS0;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void*)buf;
+            message.payloadlen = strlen(buf);
+            rc = client.publish(PUBRUNNINGTIME, message);
+        }
+        
+        
+        rc = client.subscribe(SUBNTCVOLTAGE, MQTT::QOS0, heater_regulation);
+        rc = client.subscribe(SUBRUNNINGTIME, MQTT::QOS0, wantTime);
+        wait(1);
+    }
+
+}
