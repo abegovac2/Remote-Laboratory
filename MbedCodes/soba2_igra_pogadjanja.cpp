a5295be0ae0ef031504aecba18649f53be4853a0
dzegz <85890327+dzegz@users.noreply.github.com>
2021-06-20 16:29:55 +0200

    Update soba2_igra_pogadjanja.cpp
    Bug fixes

--- MbedCodes/soba2_igra_pogadjanja.cpp
+++ MbedCodes/soba2_igra_pogadjanja.cpp
@@ -3,8 +3,11 @@
 // SOBA2: Fun room
 //
 
-#define SUBGUESS "theme/guess"
-#define PUBGAMEINFO "theme/info"
+#define SUBSETUP "project225883/us/etf/message/soba2/setup/mbed"
+#define SUBGUESS "project225883/us/etf/message/soba1/mbed/guess"
+
+#define PUBSETUP "project225883/us/etf/message/soba2/setup/mbed"
+#define PUBGAMEINFO "project225883/us/etf/message/soba1/mbed/info"
 
 #include "mbed.h"
 #define MQTTCLIENT_QOS2 0
@@ -24,6 +27,12 @@ int attempts[2]={0,0};
 int hits[2]={0,0};
 
 int matrix[25];
+
+bool data_to_send=false;
+
+void subsetup_fun(MQTT::MessageData& md){
+    data_to_send=true;
+}
 
 void reset_game(){
     winner=-1;
@@ -68,7 +77,9 @@ void guessing(MQTT::MessageData& md)
 int main(int argc, char* argv[])
 {
     reset_game();
-
+    printf("Ugradbeni sistemi\r\n");
+    printf("SOBA 2: Igra pogadjanja\r\n\r\n");
+    
     NetworkInterface *network;
     network = NetworkInterface::get_default_instance();
     
@@ -97,6 +108,9 @@ int main(int argc, char* argv[])
 
     if ((rc = client.subscribe(SUBGUESS, MQTT::QOS2, guessing)) != 0)
         printf("rc from MQTT subscribe is %d\r\n", rc);
+    
+     if ((rc = client.subscribe(SUBSETUP, MQTT::QOS2, subsetup_fun)) != 0)//
+        printf("rc from MQTT subscribe is %d\r\n", rc);
 
     MQTT::Message message;
 
@@ -104,7 +118,7 @@ int main(int argc, char* argv[])
     char buf[100];
     while(1) {
         if (!gameon) {
-            if(winner!=-1) sprintf(buf, "{\"Pobjednik igrac\": %d}", winner);
+            if(winner!=-1) sprintf(buf, "{\"Pobjednik\": \"Pobjednik igrac: %d\"}", winner);
             else sprintf(buf, "{\"Nerijeseno\"}");
             message.qos = MQTT::QOS0;
             message.retained = false;
@@ -115,7 +129,7 @@ int main(int argc, char* argv[])
             reset_game();
         }
         if(pause_game && gameon){
-            sprintf(buf, "{\"Trenutno stanje\"}");
+            sprintf(buf, "{\"Stanje\" \"Trenutno stanje: \"}");
             message.qos = MQTT::QOS0;
             message.retained = false;
             message.dup = false;
@@ -124,7 +138,7 @@ int main(int argc, char* argv[])
             rc = client.publish(PUBGAMEINFO, message);
             int red=1;        
             for(int i=0;i<25;i+=5){
-                sprintf(buf, "{\"Red %d\": %d %d %d %d %d}", red++, matrix[i], matrix[i+1], matrix[i+2], matrix[i+3], matrix[i+4]);
+                sprintf(buf, "{\"Message\" \"Red %d: %d %d %d %d %d\"}", red++, matrix[i], matrix[i+1], matrix[i+2], matrix[i+3], matrix[i+4]);
                 message.qos = MQTT::QOS0;
                 message.retained = false;
                 message.dup = false;
@@ -133,7 +147,7 @@ int main(int argc, char* argv[])
                 rc = client.publish(PUBGAMEINFO, message);
                 wait(0.01);
             }
-            sprintf(buf, "{\"Rezultat\": Igrac 0: %d pogodjenih | Igrac 1: %d pogodjenih}", hits[0], hits[1]);
+            sprintf(buf, "{\"Rezultat\": \"Igrac 0: %d pogodjenih | Igrac 1: %d pogodjenih\"}", hits[0], hits[1]);
                 message.qos = MQTT::QOS0;
                 message.retained = false;
                 message.dup = false;
@@ -142,6 +156,18 @@ int main(int argc, char* argv[])
                 rc = client.publish(PUBGAMEINFO, message);
             pause_game=false;
         }
+        if(data_to_send){
+            data_to_send=false;
+            sprintf(buf, "{\"Topics\": [\"guess\",\"info\"]}");
+            message.qos = MQTT::QOS0;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void*)buf;
+            message.payloadlen = strlen(buf);
+            rc = client.publish(PUBSETUP, message);
+        }    
+        
+        rc = client.subscribe(SUBSETUP, MQTT::QOS0, subsetup_fun);
         rc = client.subscribe(SUBGUESS, MQTT::QOS0, guessing);
         wait(1);
     }
