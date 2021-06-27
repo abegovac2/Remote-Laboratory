301d46d9f1b98d23781b5b744991631cb3ee8f08
dzegz <85890327+dzegz@users.noreply.github.com>
2021-06-27 23:18:29 +0200

    Comments added

--- MbedCodes/soba1_dvopolozajna_regulacija.cpp
+++ MbedCodes/soba1_dvopolozajna_regulacija.cpp
@@ -25,25 +25,25 @@
 Ticker check30min;
 Timer time_running;
 DigitalOut led1(p7);
-int stanje=23;
 AnalogOut dc_dimmer(p18);
+
+int stanje=23;
 float v_ntc;
 bool heater=false;
 int block_of_30min=0;
 bool mqqt_wants_time=false;
 bool data_to_send=false;
 
-void subsetup_fun(MQTT::MessageData& md){
+void subsetup_fun(MQTT::MessageData& md){ //user requires room's info
     data_to_send=true;
     printf("Zabiljezena poruka\n");
 }
 
 
-void heater_regulation(MQTT::MessageData& md)
-{
+void heater_regulation(MQTT::MessageData& md){ // 2_position_regulator logic
     /*
-    temperatura=1/0.04799 * ln(2.131*10^4/((v_ntc*673)/(3.3-v_ntc)))
-    look-up:
+    temperature=1/0.04799 * ln(2.131*10^4/((v_ntc*673)/(3.3-v_ntc)))
+    look-up table:
     23C -> Vntc = 3.01305 V
     25C -> Vntc = 2.98689 V
     27C -> Vntc = 2.95861 V
@@ -70,29 +70,29 @@ void heater_regulation(MQTT::MessageData& md)
     }
 }
 
-void wantTime(MQTT::MessageData& md){
+void wantTime(MQTT::MessageData& md){// user requires running system time
     // kod
     mqqt_wants_time=true;
     printf("Korisnicki upit za vrijeme primljen\n");
 }
 
-int running_time_in_minutes(){
+int running_time_in_minutes(){ // returns running system time
     return block_of_30min*30+time_running.read()/60;
 }
 
-void check30min_fun(){
+void check30min_fun(){ // checks if 30mins has passed
     time_running.stop();
     block_of_30min++;
     time_running.start();
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
     time_running.start();
     check30min.attach(&check30min_fun, 30*60);
     printf("Ugradbeni sistemi\r\n");
     printf("SOBA 1: Dvopoložajna regulacija\r\n\r\n");
 
+    //mqtt setup
     NetworkInterface *network;
     network = NetworkInterface::get_default_instance();
     
@@ -101,7 +101,6 @@ int main(int argc, char* argv[])
     }
 
     MQTTNetwork mqttNetwork(network);
-
     MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
     const char* hostname = "broker.hivemq.com";
@@ -116,6 +115,7 @@ int main(int argc, char* argv[])
     data.clientID.cstring = "proizvoljno";
     data.username.cstring = "";
     data.password.cstring = "";
+    ////////////////////////////////////////////////////////
     if ((rc = client.connect(data)) != 0)
         printf("rc from MQTT connect is %d\r\n", rc);
 
@@ -127,7 +127,7 @@ int main(int argc, char* argv[])
     
     if ((rc = client.subscribe(SUBSETUP, MQTT::QOS2, subsetup_fun)) != 0)//
         printf("rc from MQTT subscribe is %d\r\n", rc);
-        
+      
     MQTT::Message message;
 
     // QoS 0
@@ -138,7 +138,7 @@ int main(int argc, char* argv[])
         if(heater) dc_dimmer=1; //output 3.3V for DC dimmer INPUT
         else dc_dimmer=0;
         
-        if (old_heater!=heater) {
+        if (old_heater!=heater) { // info message for heater state change
             old_heater=heater;
             if(heater) sprintf(buf, "{\"Message\": \"[mbed]: Grijac upaljen (%d C)\"}",stanje);
             else sprintf(buf, "{\"Message\": \"[mbed]: Grijac ugasen (%d C)\"}",stanje);
@@ -151,7 +151,7 @@ int main(int argc, char* argv[])
             printf("Poslana info poruka o grijacu\n");
             stanje=23;
         }
-        if(mqqt_wants_time){
+        if(mqqt_wants_time){ // info message for system running time
             mqqt_wants_time=false;
             sprintf(buf, "{\"Message\": \"[mbed]: %d minuta od zadnjeg restarta\"}", running_time_in_minutes());
             message.qos = MQTT::QOS0;
@@ -162,10 +162,9 @@ int main(int argc, char* argv[])
             rc = client.publish(PUBRUNNINGTIME, message);
             printf("Poslana info poruka o vremenu (%d)\n",running_time_in_minutes());
         }
-        if(data_to_send){
+        if(data_to_send){ // global info message
             data_to_send=false;
-            sprintf(buf, "{\"Message\": \"[mbed]: Teme: heater, time\"}");
-            
+            sprintf(buf, "{\"Message\": \"[mbed]: Teme: heater, time\"}");      
             message.qos = MQTT::QOS0;
             message.retained = false;
             message.dup = false;
